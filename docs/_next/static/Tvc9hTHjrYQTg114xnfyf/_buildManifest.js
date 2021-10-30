14bfeafae8ec251d56fc6fa1efff73aced4106d6
JollyGrin <jollygrin@protonmail.com>
2021-10-30 13:59:05 +0200

    update

--- docs/_next/static/Tvc9hTHjrYQTg114xnfyf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/58a819c68bad9d94.css","static/chunks/pages/index-42d55ab491e300c9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,13 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
+
 
 
 
 export default function Index({ youtube, preview }) {
   return (
+    <div>
 
-    <div className={styles.container}>
-
-      <Head>
-        <title>innkeeper.eth</title>
-        <meta name="description" content="Gather 'round and stay awhile" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          the <a href="#">innkeeper</a>
-        </h1>
-
-        <div className={styles.grid}>
-          <a href="#" className={styles.card}>
-            <h2>roadmap &rarr;</h2>
-            <p>The plan to provide services in decentraland. </p>
-          </a>
-
-          <a href="#" className={styles.card}>
-            <h2>treasury &rarr;</h2>
-            <p>Assets and loot in the keepers vault.</p>
-          </a>
-
-          <a href="#" className={styles.card} >
-            <h2>the inn &rarr;</h2>
-            <p>Drop off your bags and gather &apos; round the fire.</p>
-          </a>
-
-          <a href="#" className={styles.card}  >
-            <h2>services &rarr;</h2>
-            <p>
-              full menu of services and options coming soon
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by // {' '}
-          <span>
-            Aragon DAO, Decentraland, ETH
-          </span>
-        </a>
-      </footer>
+      <p>innkeeper</p>
     </div>
   )
 }
