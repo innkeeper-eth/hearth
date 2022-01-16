c8030488dc228a5a1c482db9ef3294215350958a
JollyGrin <jollygrin@protonmail.com>
2022-01-16 20:11:06 +0100

    update max records

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,9 @@ import Hero from '../components/Hero';
 import Events from '../components/Events';
 import fetch from 'axios';
 import Inn from '../components/Inn';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 import getArtist from '../lib/airtable';
 import refactorData from '../lib/airtable';
-
 
 function HomePage({ data }) {
   return (
@@ -15,6 +14,7 @@ function HomePage({ data }) {
       <Flex justifyContent='center'>
         <Inn />
       </Flex>
+
     </>
   );
 }
@@ -23,9 +23,9 @@ export async function getServerSideProps() {
   const airtable = `https://api.airtable.com/v0/appwUCl22CLGExUBy/Events%20Page?api_key=keyppY0G5BP9W5rH8`;
   const response = await fetch(airtable);
   const { data } = response;
-  const artistsUrl = `https://api.airtable.com/v0/appwUCl22CLGExUBy/Artists?maxRecords=10&view=Grid%20view`;
+  const artistsUrl = `https://api.airtable.com/v0/appwUCl22CLGExUBy/Artists?maxRecords=20&view=Grid%20view`;
   const artistResponse = await fetch(artistsUrl, {
-    headers: { authorization: 'Bearer keyppY0G5BP9W5rH8' }
+    headers: { authorization: 'Bearer keyppY0G5BP9W5rH8' },
   });
 
   data.artists = artistResponse.data.records;
@@ -38,12 +38,11 @@ export async function getServerSideProps() {
   });
   data.combined = data.combined;
 
-  data.combined = data.combined.filter(event => {
+  data.combined = data.combined.filter((event) => {
     const unixToday = Date.parse(new Date());
     const unixEvent = Date.parse(event.fields.eventStart);
     return unixToday < unixEvent;
   });
-
 
   return {
     props: { data },
@@ -51,5 +50,3 @@ export async function getServerSideProps() {
 }
 
 export default HomePage;
-
-
