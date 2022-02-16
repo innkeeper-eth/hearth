914efcae7229f415337172174710ca307c120c41
JollyGrin <jollygrin@protonmail.com>
2022-02-16 21:47:57 +0100

    update

---
+++ components/Staff.js
@@ -0,0 +1,15 @@
+<Flex>
+    <Heading
+        as='h1'
+        size='2xl'
+        margin='0 auto'
+        pt='1em'
+        fontFamily='Zen Antique, serif'
+        color={'#F7B05B'}
+    >
+        Staff
+</Heading>
+    <Flex>
+
+    </Flex>
+</Flex>
